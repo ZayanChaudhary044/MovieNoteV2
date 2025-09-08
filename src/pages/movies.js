612c2e4c003e737a37763321cf4ad0f16c0f3f50
Zayan Chaudhary <ec23095@qmul.ac.uk>
2025-09-08 23:00:56 +0100

    cleaning up changes

--- src/pages/movies.js
+++ src/pages/movies.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 // Enhanced Movies Component with better styling
 const Movies = ({ addToList = () => {} }) => {
